2b186a0ccd30908a789db97429ff303973c6e704
Emil <emil-absalyam.off@yandex.ru>
2022-10-06 18:09:14 +0300

    fix

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+//imports
+require('dotenv').config()
 const express = require('express')
-const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
+//create
+const app = express()
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+
+
+//use
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(3000)
+app.listen(process.env.PORT)
